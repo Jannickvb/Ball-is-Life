f1fdce1e76f66a012a3adb2501b9099db58872a3
Jannick van Ballegooijen <jvbdesignz@gmail.com>
2015-05-10 23:05:49 +0200

    .

--- src/model/Entity.java
+++ src/model/Entity.java
@@ -2,7 +2,7 @@ package model;
 
 import java.awt.Graphics2D;
 
-public abstract class MabObject {
+public abstract class Entity {
 	public abstract void init();
 	public abstract void draw(Graphics2D g2);
 	public abstract void update();
